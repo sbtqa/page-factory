a38da90a87a540307e9eda6c23ad91b140b94f7f
Konstantin Maltsev <mkypers@gmail.com>
2017-01-26 09:25:23 +0700

    add appium props; add logs

--- src/main/java/ru/sbtqa/tag/pagefactory/drivers/MblDriver.java
+++ src/main/java/ru/sbtqa/tag/pagefactory/drivers/MblDriver.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.sbtqa.tag.pagefactory.PageFactory;
 import static ru.sbtqa.tag.pagefactory.PageFactory.setAspectsDisabled;
 import ru.sbtqa.tag.pagefactory.exceptions.FactoryRuntimeException;
@@ -14,48 +16,52 @@ import ru.sbtqa.tag.qautils.properties.Props;
 import ru.sbtqa.tag.videorecorder.VideoRecorder;
 
 public class MblDriver {
-    
+
+    private static final Logger LOG = LoggerFactory.getLogger(MblDriver.class);
+
     private static AppiumDriver<AndroidElement> mobileDriver;
-    
+
     public static AppiumDriver<AndroidElement> getDriver() {
-        if (null == mobileDriver) {
+	if (null == mobileDriver) {
 	    if (Boolean.valueOf(Props.get("video.enable"))) {
-                VideoRecorder.getInstance().startRecording();
-            }
-	    
-            createDriver();
-        }
-        return mobileDriver;
+		VideoRecorder.getInstance().startRecording();
+	    }
+
+	    createDriver();
+	}
+	return mobileDriver;
     }
 
     private static void createDriver() {
-        File classpathRoot = new File(System.getProperty("user.dir"));
-        //TODO refactor app get
-        File appDir = new File(classpathRoot, "app");
-        File app = new File(appDir, "SberbankMobile.apk");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        //TODO move to application properties
-        capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability("platformVersion", "6.0");
-        capabilities.setCapability("app", app.getAbsolutePath());
-        capabilities.setCapability("appPackage", "ru.sberbankmobile");
-        capabilities.setCapability("appActivity", "SplashActivity");
-        capabilities.setCapability("autoGrantPermissions", "true");
+	File appDir = new File(System.getProperty("user.dir"), "app");
+	File app = new File(appDir, Props.get("appium.app.name"));
+	LOG.info("Absolute path to app is {}", app.getAbsolutePath());
 
-        URL url;
-        try {
-            url = new URL(PageFactory.getInitialUrl());
-        } catch (MalformedURLException e) {
-            throw new FactoryRuntimeException("Failed to connect to appium on url " + PageFactory.getInitialUrl(), e);
-        }
+	DesiredCapabilities capabilities = new DesiredCapabilities();
+	capabilities.setCapability("deviceName", Props.get("appium.device.name"));
+	capabilities.setCapability("platformVersion", Props.get("appium.device.platform"));
+	capabilities.setCapability("app", app.getAbsolutePath());
+	capabilities.setCapability("appPackage", Props.get("appium.app.package"));
+	capabilities.setCapability("appActivity", Props.get("appium.app.activity"));
+	capabilities.setCapability("autoGrantPermissions", "true");
+	LOG.info("Capabilities are {}", capabilities);
 
-        setAspectsDisabled(true);
-        mobileDriver = new AndroidDriver<>(url, capabilities);
+	URL url;
+	try {
+	    url = new URL(PageFactory.getInitialUrl());
+	} catch (MalformedURLException e) {
+	    throw new FactoryRuntimeException("Failed to connect to appium on url " + PageFactory.getInitialUrl(), e);
+	}
+
+	setAspectsDisabled(true);
+	LOG.info("Aspect disabled");
+	mobileDriver = new AndroidDriver<>(url, capabilities);
+	LOG.info("Mobile driver created {}", mobileDriver);
     }
-    
+
     public static void dispose() {
-        if (mobileDriver != null) {
-            mobileDriver.quit();
-        }
+	if (mobileDriver != null) {
+	    mobileDriver.quit();
+	}
     }
 }
