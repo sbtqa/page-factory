793f6a48d113070f4a18f6703fd244f27083374f
0I0u3uk <svetlakovanton@gmail.com>
2017-05-22 16:34:58 +0700

    Fixed for Chuck

--- src/main/java/ru/sbtqa/tag/pagefactory/support/OnFailureScheduler.java
+++ src/main/java/ru/sbtqa/tag/pagefactory/support/OnFailureScheduler.java
@@ -1,8 +1,5 @@
 package ru.sbtqa.tag.pagefactory.support;
 
-import ru.sbtqa.tag.allurehelper.ParamsHelper;
-import ru.sbtqa.tag.qautils.properties.Props;
-import ru.sbtqa.tag.videorecorder.VideoRecorder;
 import ru.yandex.qatools.allure.cucumberjvm.callback.OnFailureCallback;
 
 public class OnFailureScheduler implements OnFailureCallback {
