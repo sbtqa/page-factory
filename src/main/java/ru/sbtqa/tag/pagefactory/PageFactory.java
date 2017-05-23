cd29d49bd7e4ff108909be751bcc5bdc1a07a8ab
ScorpDS <notasimplename@gmail.com>
2017-05-23 19:32:04 +0700

    cleanup#2

--- src/main/java/ru/sbtqa/tag/pagefactory/PageFactory.java
+++ src/main/java/ru/sbtqa/tag/pagefactory/PageFactory.java
@@ -93,46 +93,7 @@ public class PageFactory {
     public static PageWrapper getInstance() {
         if (null == PageWrapper) {
             PageWrapper = new PageWrapper(PAGES_PACKAGE);
-
-            Reflections reflections;
-            reflections = new Reflections(PageFactory.getPagesPackage());
-
-            Collection<String> allClassesString = reflections.getStore().get("SubTypesScanner").values();
-            Set<Class<?>> allClasses = new HashSet();
-            for (String clazz : allClassesString) {
-                try {
-                    allClasses.add(Class.forName(clazz));
-                } catch (ClassNotFoundException e) {
-                    LOG.warn("Cannot add all classes to set from package storage", e);
-                }
-            }
-
-            for (Class<?> page : allClasses) {
-                List<Class> supers = ClassUtilsExt.getSuperclassesWithInheritance(page);
-                if (!supers.contains(Page.class) && !supers.contains(HtmlElement.class)) {
-                    if (page.getName().contains("$")) {
-                        continue; //We allow private additional classes but skip it if its not extends Page
-                    } else {
-                        throw new FactoryRuntimeException("Class " + page.getName() + " is not extended from Page class. Check you webdriver.pages.package property.");
-                    }
-                }
-                List<Field> fields = FieldUtilsExt.getDeclaredFieldsWithInheritance(page);
-                Map<Field, String> fieldsMap = new HashMap<>();
-                for (Field field : fields) {
-                    Class<?> fieldType = field.getType();
-                    if (fieldType.equals(WebElement.class)) {
-
-                        ElementTitle titleAnnotation = field.getAnnotation(ElementTitle.class);
-                        if (titleAnnotation != null) {
-                            fieldsMap.put(field, titleAnnotation.value());
-                        } else {
-                            fieldsMap.put(field, field.getName());
-                        }
-                    }
-                }
-
-                PageFactory.getPageRepository().put((Class<? extends Page>) page, fieldsMap);
-            }
+            initializeFieldsMap();
         }
         return PageWrapper;
     }
@@ -220,6 +181,48 @@ public class PageFactory {
                 } else {
                     throw new FactoryRuntimeException("Environment '" + ENVIRONMENT + "' is not supported");
                 }
+        }
+    }
+
+    private static void initializeFieldsMap() {
+        Reflections reflections;
+        reflections = new Reflections(PageFactory.getPagesPackage());
+
+        Collection<String> allClassesString = reflections.getStore().get("SubTypesScanner").values();
+        Set<Class<?>> allClasses = new HashSet();
+        for (String clazz : allClassesString) {
+            try {
+                allClasses.add(Class.forName(clazz));
+            } catch (ClassNotFoundException e) {
+                LOG.warn("Cannot add all classes to set from package storage", e);
+            }
+        }
+
+        for (Class<?> page : allClasses) {
+            List<Class> supers = ClassUtilsExt.getSuperclassesWithInheritance(page);
+            if (!supers.contains(Page.class) && !supers.contains(HtmlElement.class)) {
+                if (page.getName().contains("$")) {
+                    continue; //We allow private additional classes but skip it if its not extends Page
+                } else {
+                    throw new FactoryRuntimeException("Class " + page.getName() + " is not extended from Page class. Check you webdriver.pages.package property.");
+                }
+            }
+            List<Field> fields = FieldUtilsExt.getDeclaredFieldsWithInheritance(page);
+            Map<Field, String> fieldsMap = new HashMap<>();
+            for (Field field : fields) {
+                Class<?> fieldType = field.getType();
+                if (fieldType.equals(WebElement.class)) {
+
+                    ElementTitle titleAnnotation = field.getAnnotation(ElementTitle.class);
+                    if (titleAnnotation != null) {
+                        fieldsMap.put(field, titleAnnotation.value());
+                    } else {
+                        fieldsMap.put(field, field.getName());
+                    }
+                }
+            }
+
+            PageFactory.getPageRepository().put((Class<? extends Page>) page, fieldsMap);
         }
     }
 }
--- src/main/java/ru/sbtqa/tag/pagefactory/stepdefs/SetupStepDefs.java
+++ src/main/java/ru/sbtqa/tag/pagefactory/stepdefs/SetupStepDefs.java
@@ -4,33 +4,18 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.log4j.PropertyConfigurator;
-import org.openqa.selenium.WebElement;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.sbtqa.tag.allure.TagAllureReporter;
 import ru.sbtqa.tag.allurehelper.ParamsHelper;
-import ru.sbtqa.tag.pagefactory.Page;
 import ru.sbtqa.tag.pagefactory.PageFactory;
-import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
 import ru.sbtqa.tag.pagefactory.drivers.TagWebDriver;
-import ru.sbtqa.tag.pagefactory.exceptions.FactoryRuntimeException;
 import ru.sbtqa.tag.pagefactory.support.Environment;
 import ru.sbtqa.tag.pagefactory.support.OnFailureScheduler;
 import ru.sbtqa.tag.qautils.properties.Props;
-import ru.sbtqa.tag.qautils.reflect.ClassUtilsExt;
-import ru.sbtqa.tag.qautils.reflect.FieldUtilsExt;
 import ru.sbtqa.tag.videorecorder.VideoRecorder;
-import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
 public class SetupStepDefs {
 
