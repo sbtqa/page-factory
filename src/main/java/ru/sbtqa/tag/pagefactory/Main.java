7ee26726381e6cc9c4b9a1eab7a1618f0f061b0f
Konstantin Maltsev <mkypers@gmail.com>
2018-04-05 16:20:38 +0700

    remove main

--- src/main/java/ru/sbtqa/tag/pagefactory/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.sbtqa.tag.pagefactory;
-
-import java.util.Locale;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Locale.getDefault());
-    }
-}
