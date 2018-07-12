0a9f4def33e12c4bf524887e4d79efe4522fd150
Сидоченко Виктор Викторович <VVSidochenko.SBT@sberbank.ru>
2018-07-12 16:37:04 +0700

    Works with tags

--- src/main/java/ru/sbtqa/tag/pagefactory/aspects/DataAspect.java
+++ src/main/java/ru/sbtqa/tag/pagefactory/aspects/DataAspect.java
@@ -5,33 +5,37 @@ import gherkin.ast.Feature;
 import gherkin.ast.GherkinDocument;
 import gherkin.ast.ScenarioDefinition;
 import gherkin.ast.Step;
+import gherkin.ast.Tag;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import ru.sbtqa.tag.datajack.TestDataObject;
 import ru.sbtqa.tag.datajack.exceptions.DataException;
 import ru.sbtqa.tag.pagefactory.support.DataProvider;
 
 @Aspect
 public class DataAspect {
 
-    @Pointcut("call(* *.addChildren(..))")
-    public void addChildren() {
-    }
+    private String featureDataTag;
+    private String currentScenarioTag;
 
-    @Around("addChildren()")
+    @Around("call(* *.addChildren(..))")
     public void stash(ProceedingJoinPoint joinPoint) throws Throwable {
         List<CucumberFeature> cucumberFeatures = (List<CucumberFeature>) joinPoint.getArgs()[0];
         for (CucumberFeature cucumberFeature : cucumberFeatures) {
             GherkinDocument gherkinDocument = cucumberFeature.getGherkinFeature();
             Feature feature = gherkinDocument.getFeature();
-            feature.getTags();
+            featureDataTag = parseTags(feature.getTags());
             List<ScenarioDefinition> featureChildren = feature.getChildren();
             for (ScenarioDefinition scenarioDefinition : featureChildren) {
+                List<Tag> currentScenarioTags = getScenarioTags(scenarioDefinition);
+                currentScenarioTag = parseTags(currentScenarioTags);
                 List<Step> steps = scenarioDefinition.getSteps();
                 for (Step step : steps) {
                     FieldUtils.writeField(step, "text", parseString(step.getText()), true);
@@ -41,6 +45,19 @@ public class DataAspect {
         }
 
         joinPoint.proceed();
+    }
+
+    private List<Tag> getScenarioTags(ScenarioDefinition scenarioDefinition) {
+        try {
+            return (List<Tag>) FieldUtils.readField(scenarioDefinition, "tags", true);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private String parseTags(List<Tag> tags) throws DataException {
+        Optional<Tag> dataTag = tags.stream().filter(predicate -> predicate.getName().startsWith("@data")).findFirst();
+        return dataTag.isPresent() ? dataTag.get().getName().split("=")[1].trim() : null;
     }
 
     private String parseString(String raw) throws DataException {
@@ -61,7 +78,10 @@ public class DataAspect {
                 sb = sb.replace(m.start(1) + sbSkip, m.end(1) + sbSkip, "");
                 sbSkip += "".length() - collection.length();
             } else {
-                DataProvider.updateCollection(DataProvider.getInstance().fromCollection(DataProvider.getConfigCollection()));
+                String tag = currentScenarioTag != null ? currentScenarioTag : featureDataTag;
+                if (tag != null) {
+                    parseTestDataObject(tag);
+                }
             }
 
             String dataPath = value.replace("${", "").replace("}", "");
@@ -73,4 +93,21 @@ public class DataAspect {
         return sb.toString();
     }
 
+    private void parseTestDataObject(String raw) throws DataException {
+        Pattern dataP = Pattern.compile("([^\\$]+)(?:\\$\\{([^\\}]+)\\})?");
+        Matcher m = dataP.matcher(raw.trim());
+
+        if (m.matches()) {
+            String collection = m.group(1);
+            String value = m.group(2);
+
+            TestDataObject tdo = DataProvider.getInstance().fromCollection(collection);
+
+            if (value != null) {
+                tdo = tdo.get(value);
+            }
+
+            DataProvider.updateCollection(tdo);
+        }
+    }
 }
