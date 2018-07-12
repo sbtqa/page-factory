7cf24e30689eca73f8c95260fabadd82d657a7cb
Сидоченко Виктор Викторович <VVSidochenko.SBT@sberbank.ru>
2018-07-12 14:27:11 +0700

    refactoring

--- src/main/java/ru/sbtqa/tag/pagefactory/aspects/DataAspect.java
+++ src/main/java/ru/sbtqa/tag/pagefactory/aspects/DataAspect.java
@@ -13,14 +13,37 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import ru.sbtqa.tag.datajack.TestDataObject;
 import ru.sbtqa.tag.datajack.exceptions.DataException;
 import ru.sbtqa.tag.pagefactory.support.DataProvider;
 
 @Aspect
 public class DataAspect {
 
-    String parseString(String raw) throws DataException {
+    @Pointcut("call(* *.addChildren(..))")
+    public void addChildren() {
+    }
+
+    @Around("addChildren()")
+    public void stash(ProceedingJoinPoint joinPoint) throws Throwable {
+        List<CucumberFeature> cucumberFeatures = (List<CucumberFeature>) joinPoint.getArgs()[0];
+        for (CucumberFeature cucumberFeature : cucumberFeatures) {
+            GherkinDocument gherkinDocument = cucumberFeature.getGherkinFeature();
+            Feature feature = gherkinDocument.getFeature();
+            feature.getTags();
+            List<ScenarioDefinition> featureChildren = feature.getChildren();
+            for (ScenarioDefinition scenarioDefinition : featureChildren) {
+                List<Step> steps = scenarioDefinition.getSteps();
+                for (Step step : steps) {
+                    FieldUtils.writeField(step, "text", parseString(step.getText()), true);
+                }
+
+            }
+        }
+
+        joinPoint.proceed();
+    }
+
+    private String parseString(String raw) throws DataException {
         Pattern dataP = Pattern.compile("(?:(@[^\\$]+)?(\\$\\{[^\\}]+\\}))+");
         Matcher m = dataP.matcher(raw);
         StringBuffer sb = new StringBuffer(raw);
@@ -40,15 +63,7 @@ public class DataAspect {
             } else {
                 DataProvider.updateCollection(DataProvider.getInstance().fromCollection(DataProvider.getConfigCollection()));
             }
-            TestDataObject deepObj = DataProvider.getInstance().get("my.long.super.path.1.2.3.4.5");// ->name
-            TestDataObject deepObj2 = DataProvider.getInstance().get("my.long2.super.path.1.2.3.4.6");// -> surname
-            
-            
-            deepObj.get("name").getValue();
-            deepObj2.get("surname").getValue();
-            
-            
-            
+
             String dataPath = value.replace("${", "").replace("}", "");
             String parsedValue = DataProvider.getInstance().get(dataPath).getValue();
             sb = sb.replace(m.start(2) + sbSkip, m.end(2) + sbSkip, parsedValue);
@@ -56,29 +71,6 @@ public class DataAspect {
 
         }
         return sb.toString();
-    }
-
-    @Pointcut("call(* *.addChildren(..))")
-    public void addChildren() {
-    }
-
-    @Around("addChildren()")
-    public void stash(ProceedingJoinPoint joinPoint) throws Throwable {
-        List<CucumberFeature> cucumberFeatures = (List<CucumberFeature>) joinPoint.getArgs()[0];
-        for (CucumberFeature cucumberFeature : cucumberFeatures) {
-            GherkinDocument gherkinDocument = cucumberFeature.getGherkinFeature();
-            Feature feature = gherkinDocument.getFeature();
-            List<ScenarioDefinition> featureChildren = feature.getChildren();
-            for (ScenarioDefinition scenarioDefinition : featureChildren) {
-                List<Step> steps = scenarioDefinition.getSteps();
-                for (Step step : steps) {
-                    FieldUtils.writeField(step, "text", parseString(step.getText()), true);
-                }
-
-            }
-        }
-
-        joinPoint.proceed();
     }
 
 }
